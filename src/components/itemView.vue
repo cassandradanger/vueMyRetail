867c19b75e5c0fb6b8a1123429d083bf1bd037bd
Casie Lynch <casie@primeacademy.io>
2018-01-14 09:46:15 -0600

    start photo gallery

--- src/components/itemView.vue
+++ src/components/itemView.vue
@@ -1,49 +1,59 @@
 <template>
   <div id="itemView">
-    <h1 class="itemTitle">{{itemData.CatalogEntryView[0].title}}</h1>
-    <h2 class="price">{{itemData.CatalogEntryView[0].Offers[0].OfferPrice[0].formattedPriceValue}} <span class="priceQualifier">{{itemData.CatalogEntryView[0].Offers[0].OfferPrice[0].priceQualifier}}</span></h2>
-    <div class="promotions" v-for="promotion in itemData.CatalogEntryView[0].Promotions">
-        <h3 v-for="description in promotion.Description">{{description.shortDescription}}</h3>
-    </div>
-    <div class="quantityCounter">{QUANTITY COUNTER GOES HERE}</div>
-    <div class="buttons">
-        <button class="pickUp">PICK UP IN STORE</button>
-        <button class="addToCart">ADD TO CART</button>
-    </div>
-    <h3 class="returns">returns | <span class="returnPolicy">This item must be returned within 30 days of the ship date. See {return policy} for details. Prices, promotions, styles and availibility may vary by store and online.</span></h3>
-    <div class="buttons">
-        <button>ADD TO REGISTRY</button>
-        <button>ADD TO LIST</button>
-        <button>SHARE</button>
-    </div>
-    <h4 v-for="feature in features">{{feature}}</h4>
-    <div class="overallReviews">
-        <div class="starRating">*****</div>
-        <div class="viewReviews"> view all {{this.itemData.CatalogEntryView[0].CustomerReview[0].Reviews.length}} reviews </div>
-    </div>
-    <div class="reviews">
-        <div class="pro">
-            <h3 class="reviewTitle">Pro</h3>
-            <h5 class="reviewDescription">most helpful 4-5 star review</h5>
-            <h4>{{this.itemData.CatalogEntryView[0].CustomerReview[0].Pro[0].title}}</h4>
-            <h5>{{this.itemData.CatalogEntryView[0].CustomerReview[0].Pro[0].review}}</h5>
-        
+    <div class="leftSide">
+        <h1 class="itemTitle">{{itemData.CatalogEntryView[0].title}}</h1>
+        <h2 class="price">{{itemData.CatalogEntryView[0].Offers[0].OfferPrice[0].formattedPriceValue}} <span class="priceQualifier">{{itemData.CatalogEntryView[0].Offers[0].OfferPrice[0].priceQualifier}}</span></h2>
+        <div class="promotions" v-for="promotion in itemData.CatalogEntryView[0].Promotions">
+            <h3 v-for="description in promotion.Description">{{description.shortDescription}}</h3>
         </div>
-        <div class="con">
-            <h3 class="reviewTitle">Con</h3>
-            <h5 class="reviewDescription">most helpful 1-2 star review</h5>
-            <h4>{{this.itemData.CatalogEntryView[0].CustomerReview[0].Con[0].title}}</h4>
-            <h5>{{this.itemData.CatalogEntryView[0].CustomerReview[0].Con[0].review}}</h5>
+        <photo-slider></photo-slider>
+        <div class="overallReviews">
+            <div class="starRating">*****</div>
+            <div class="viewReviews"> view all {{this.itemData.CatalogEntryView[0].CustomerReview[0].Reviews.length}} reviews </div>
+        </div>
+        <div class="reviews">
+            <div class="pro">
+                <h3 class="reviewTitle">Pro</h3>
+                <h5 class="reviewDescription">most helpful 4-5 star review</h5>
+                <h4>{{this.itemData.CatalogEntryView[0].CustomerReview[0].Pro[0].title}}</h4>
+                <h5>{{this.itemData.CatalogEntryView[0].CustomerReview[0].Pro[0].review}}</h5>
+            
+            </div>
+            <div class="con">
+                <h3 class="reviewTitle">Con</h3>
+                <h5 class="reviewDescription">most helpful 1-2 star review</h5>
+                <h4>{{this.itemData.CatalogEntryView[0].CustomerReview[0].Con[0].title}}</h4>
+                <h5>{{this.itemData.CatalogEntryView[0].CustomerReview[0].Con[0].review}}</h5>
 
+            </div>
         </div>
+    </div>
+    <div class="rightSide">
+        <div class="quantityCounter">{QUANTITY COUNTER GOES HERE}</div>
+        <div class="buttons">
+            <button class="pickUp">PICK UP IN STORE</button>
+            <button class="addToCart">ADD TO CART</button>
+        </div>
+        <h3 class="returns">returns | <span class="returnPolicy">This item must be returned within 30 days of the ship date. See {return policy} for details. Prices, promotions, styles and availibility may vary by store and online.</span></h3>
+        <div class="buttons">
+            <button>ADD TO REGISTRY</button>
+            <button>ADD TO LIST</button>
+            <button>SHARE</button>
+        </div>
+        <h4 v-for="feature in features">{{feature}}</h4>
     </div>
   </div>
 </template>
 
 <script>
+import PhotoSlider from './photoSlider.vue';
+
     export default {
         name: 'itemView',
         props: ['itemData'],
+        components:{
+            PhotoSlider,
+        },
         data(){
             return {
                 features: [],
@@ -70,6 +80,16 @@
 
 <style lang="scss" scoped>
     #itemView{
-        margin: 200px;
+        margin: 200px 100px 100px 225px;
+    }
+    .leftSide{
+        width: 49%;
+        float: left;
+        border: 1px solid black;
+    }
+    .rightSide{
+        width: 49%;
+        float: right;
+        border: 1px solid black;
     }
 </style>
---
+++ src/components/photoSlider.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="photo-slider">
+        <div class="mainImage"><img :src="this.itemData.CatalogEntryView[0].Images[0].PrimaryImage[0].image"/></div>
+        <div class="gallery">
+            <button class="back"><</button>
+            <div class="array"></div>
+            <button class="forward">></button>
+        </div>
+    </div>
+</template>
+
+<script>
+import itemData from '../assets/item-data.json';
+
+export default {
+  name: 'photo-slider',
+  components: {
+    itemData,
+  },
+  data(){
+    return {
+        itemData: itemData,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
